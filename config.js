6b33857371c21afe215afe97b375e045cb65a9ac
luthordev <luthfiathorique@gmail.com>
2022-04-20 11:40:13 +0700

    tes2

--- config.js
+++ config.js
@@ -4,12 +4,12 @@ const config = {
     app: {
       name: "NodeJS API",
       host: `http://${ip.address()}`,
-      port: 3001,
+      port: 3000,
       communication: true,
     },
     db: {
-      host: "mongodb://localhost:27017",
-      database: "db_karyausahasejahtera",
+      host: "mongodb+srv://luthor:luthfi@28@karyausahasejahtera.wlmj3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      database: "karyausahasejahtera",
       username: "",
       password: "",
     },
